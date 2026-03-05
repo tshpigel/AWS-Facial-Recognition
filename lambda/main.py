cdab9414caa48e428d371b6101e62896e6e28d06
tshpigel <96367527+tshpigel@users.noreply.github.com>
2026-03-04 20:23:12 -0800

    Add files via upload

--- lambda/main.py
+++ lambda/main.py
@@ -1,1 +1,20 @@
+import boto3
+from botocore.exceptions import ClientError
+import base64
+from imageio import imread
+import io
 
+reko = boto3.client('rekognition')
+
+def recog(event, context):
+    b64 = event.get('body')
+    dec = base64.b64decode(b64)
+    i = imread(io.BytesIO(dec))
+
+    threshold = 70
+    maxFaces = 2
+
+    compare = reko.search_faces_by_image(CollectionId="atley-collection", Image=i, FaceMatchThreshold=threshold, MaxFaces=maxFaces)
+
+    result = compare['FaceMatches']['Face']['ExternalImageId']
+    return result
